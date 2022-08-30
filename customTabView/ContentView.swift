04571ae66e6c0ba724ba7a14cb533cb82e2bffd5
junsai <junsai0530@gmail.com>
2022-08-30 18:08:10 +0900

    Initial Commit

---
+++ customTabView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  customTabView
+//
+//  Created by 小暮準才 on 2022/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ customTabView/customTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  customTabViewApp.swift
+//  customTabView
+//
+//  Created by 小暮準才 on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct customTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
